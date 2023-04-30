81ce734fd3d908eaad5993d3413a6633ce0c545e
Francesco Piccolo <piccolofrancesco300@gmail.com>
2023-04-30 20:14:53 +0200

    footer completato, fatta freccietta back to top

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="d-flex flex-column">
+    <h4 class="footertext1 line">Made downstairs. Below sea level xxx</h4>
+  
+  <div class="container m-0 p-0 line2">
+    <div class="row">
+      <div class="col-6">
+      <a href="" class="text-dark"><h4 class="footertext3">hello@downstairs.design</h4></a>
+    </div>
+      <div class="col-6 d-flex social justify-content-end footertext2">
+        <a href="" class="text-dark"><h4>Dribbble</h4></a>
+        <a href="" class="text-dark"><h4>Instagram</h4></a>
+        <a href="" class="text-dark"><h4>Linkedin</h4></a>
+      </div>
+    </div>
+  </div>
+  
+  <img src="/media/Downstairs-Design-Studio-Logo.svg" alt="logo2" class="logo2">
+  
+  <div class="linefooter"></div>
+  </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,21 +47,4 @@
     </div>
   </div>
 
-<footer class="d-flex flex-column">
-  <h4 class="footertext1 line">Made downstairs. Below sea level xxx</h4>
-
-<div class="container m-0 p-0 line2">
-  <div class="row">
-    <div class="col-6">
-    <a href="" class="text-dark"><h4 class="footertext3">hello@downstairs.design</h4></a>
-  </div>
-    <div class="col-6 d-flex social justify-content-end footertext2">
-      <a href="" class="text-dark"><h4>Dribbble</h4></a>
-      <a href="" class="text-dark"><h4>Instagram</h4></a>
-      <a href="" class="text-dark"><h4>Linkedin</h4></a>
-    </div>
-  </div>
-</div>
-</footer>
-
 </x-layout>
