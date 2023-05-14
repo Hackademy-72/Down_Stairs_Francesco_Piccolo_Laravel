cfe74fe55169666c9065bed88e385c7db981369a
Francesco Piccolo <piccolofrancesco300@gmail.com>
2023-05-14 23:36:14 +0200

    installato npm e bootstrap, composer.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,10 @@
     <title>DownStairs</title>
     {{-- bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    {{-- css --}}
-    <link rel="stylesheet" href="/css/style.css">
+    {{-- vite css/js --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js']);
   </head>
   <body>
-        
     <x-navbar></x-navbar>
 
     {{$slot}}
